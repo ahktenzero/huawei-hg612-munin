69ac4ab25643232326bf0e6596a8b1921b651b68
James TD Smith <ahktenzero@mohorovi.cc>
2019-02-14 18:25:34 +0000

    Write expect output to a temp file to ensure atomic file updates
    With the cron process to update the data file and the munin fetch process
    running the plugins operating independently, occasionally the plugins will be
    run while the file is being updated but not fully written yet which can cause
    failures.
    
    Writing the updated results from the router to a separate file and then renaming
    it guarantees the file will always be complete.

--- getstats.sh
+++ getstats.sh
@@ -7,4 +7,15 @@ CONFIG=$SCRIPTDIR/config
 statsfile=${statsfile:-/var/lib/munin/plugin-state/huawei-hg612-munin-output.txt}
 modem_ip=${modem_ip:-"192.168.1.1"}
 
-expect `dirname $0`/huawei.expect $modem_ip  | tee | tr -d "\015"  > $statsfile
+TMPFILE=`mktemp -t hg612.XXX`
+if [ $? -ne 0 ]; then
+    echo "$0: Can't create temp file, exiting..."
+   exit 1
+fi
+
+chown munin:munin $TMPFILE
+chmod g+r $TMPFILE
+
+expect `dirname $0`/huawei.expect $modem_ip  | tee | tr -d "\015"  > $TMPFILE
+
+mv $TMPFILE $statsfile
